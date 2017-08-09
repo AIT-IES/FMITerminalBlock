2e3f76f359bdb6ac7966acf86b7b1f754ce01378
Michael Spiegel <michael.spiegel@ait.ac.at>
2017-08-09 09:05:22 +0200

    ADD: Interpolating event filter (Towards #8)

--- scripts/data/filtered_reader.py
+++ scripts/data/filtered_reader.py
@@ -1,4 +1,5 @@
 from data.abc.abstract_reader import AbstractReader
+from data.simulation_data_type import SimulationDataType
 
 class EmptyEventFilter(AbstractReader):
     """Reader which removes all empty events from the source reader
@@ -42,4 +43,86 @@ class EmptyEventFilter(AbstractReader):
                     yield ev
         except StopIteration:
             pass;
+
+class ConstantInterpolationEventFilter(AbstractReader):
+    """Filteres model variables which are not present and interpolates them
+    
+    A constant interpolation function is used to fill missing model variables.
+    I.e. previous values are taken. In case a model variable was not contained 
+    in any event so far, an initial default value is taken.
+    """
+    
+    default_values = {SimulationDataType.REAL: 0.0, \
+                      SimulationDataType.INTEGER: 0, \
+                      SimulationDataType.BOOLEAN: False, \
+                      SimulationDataType.STRING: ""}
+    
+    def __init__(self, source_reader, initial_values={}):
+        """Initializes the object. 
+        
+        The source reader must be an abstract reader which deliverers a stream 
+        of events. The dict of initial values maps model variable names to their
+        initial values. It is assumed that the types of the initial values can 
+        be interpreted according to the model variable type. In case a model 
+        variable is not present in the initial_values dict, a type-dependent 
+        default value (0, 0.0, False, and "" respectively) is taken.
+        """
+        
+        self._source_reader = source_reader
+        self._initial_variable_image = self._get_initial_variable_image( \
+                initial_values, source_reader.get_header() \
+            )
+    
+    def _get_initial_variable_image(self, initial_values, header):
+        """Returns a dictionary which maps all model variables to their values
+        
+        The initial_values dict overrides the default values which depend on the
+        type of the variable. The header dict records the types of each model 
+        variable as defined by AbstractReader.get_header().
+        """
+        
+        ret = {}
+        for varname, vartype in header.items():
+            if varname in initial_values:
+                ret[varname] = initial_values[varname]
+            else:
+                de = ConstantInterpolationEventFilter.default_values[vartype]
+                ret[varname] = de
+        return ret
+    
+    def get_header(self):
+        """Returns the header of the source reader"""
+        
+        return self._source_reader.get_header()
+    
+    def __iter__(self):
+        """Returns an iterator which interpolates all model variables"""
+        
+        return self._interpolate_variables()
+    
+    def _interpolate_variables(self):
+        """Generator function which does the actual interpolation"""
+        
+        image = self._initial_variable_image
+        it = iter(self._source_reader)
+        try:
+            while True:
+                ev = next(it)
+                for variable_name in self.get_header().keys():
+                    self._sync_variable(variable_name, image, ev)
+                yield ev
+        except StopIteration:
+            pass
+    
+    def _sync_variable(self, variable_name, image, event):
+        """Synchronizes the given image dictionary and the event
+        
+        In case the event does not contain the variable_name, the event is 
+        updated, otherwise, the image dictionary is updated.
+        """
+        
+        if variable_name in event:
+            image[variable_name] = event[variable_name]
+        else:
+            event[variable_name] = image[variable_name]
     
--- scripts/data/test_filtered_reader.py
+++ scripts/data/test_filtered_reader.py
@@ -1,5 +1,7 @@
 import unittest
 from data.filtered_reader import EmptyEventFilter
+from data.filtered_reader import ConstantInterpolationEventFilter
+
 from data.reader import Reader
 
 class TestEmptyEventFilter(unittest.TestCase):
@@ -43,8 +45,90 @@ class TestEmptyEventFilter(unittest.TestCase):
         self.assertEqual(ev["x"], 0.1)
         
         self.assertRaises(StopIteration, next, it)
+
+class TestConstantInterpolationEventFilter(unittest.TestCase):
+    """Tests the interpolation event filter class instance"""
     
+    def test_get_header(self):
+        """Fetch the header of the data stream"""
+        
+        csv_data = ['"time";"x"', '"fmiReal";"fmiReal"']
+        source_reader = Reader(csv_data)
+        filtered_reader = ConstantInterpolationEventFilter(source_reader)
+        self.assertDictEqual(filtered_reader.get_header(), \
+            source_reader.get_header())
     
+    def test_default_filter_sequence(self):
+        """Test the filtering functionality without any start values"""
+        
+        csv_dat = ['"time";"a";"b";"c";"d"', \
+                   '"fmiReal";"fmiReal";"fmiInteger";"fmiBoolean";"fmiString"',\
+                   '0.0;;;;', \
+                   '0.1;0.1;;;', \
+                   '0.2;;42;;', \
+                   '0.3;;;1;', \
+                   '0.4;;;;"Hi!"', \
+                   '0.5;;;;', \
+                   '0.6;-2.0;5;0;""']
+        source_reader = Reader(csv_dat)
+        filtered_reader = ConstantInterpolationEventFilter(source_reader)
+        
+        it = iter(filtered_reader)
+        
+        ev = next(it)
+        self.assertEqual(ev.get_time(), 0.0)
+        ref = {"a":0.0, "b":0, "c":False, "d":""}
+        self.assertDictEqual(dict(ev), ref)
+        
+        ev = next(it)
+        self.assertEqual(ev.get_time(), 0.1)
+        ref = {"a":0.1, "b":0, "c":False, "d":""}
+        self.assertDictEqual(dict(ev), ref)
+        
+        ev = next(it)
+        self.assertEqual(ev.get_time(), 0.2)
+        ref = {"a":0.1, "b":42, "c":False, "d":""}
+        self.assertDictEqual(dict(ev), ref)
+        
+        ev = next(it)
+        self.assertEqual(ev.get_time(), 0.3)
+        ref = {"a":0.1, "b":42, "c":True, "d":""}
+        self.assertDictEqual(dict(ev), ref)
+        
+        ev = next(it)
+        self.assertEqual(ev.get_time(), 0.4)
+        ref = {"a":0.1, "b":42, "c":True, "d":"Hi!"}
+        self.assertDictEqual(dict(ev), ref)
+        
+        ev = next(it)
+        self.assertEqual(ev.get_time(), 0.5)
+        ref = {"a":0.1, "b":42, "c":True, "d":"Hi!"}
+        self.assertDictEqual(dict(ev), ref)
+        
+        ev = next(it)
+        self.assertEqual(ev.get_time(), 0.6)
+        ref = {"a":-2.0, "b":5, "c":False, "d":""}
+        self.assertDictEqual(dict(ev), ref)
+        
+        self.assertRaises(StopIteration, next, it)
+    
+    def test_initialized_filter_sequence_1(self):
+        """Test the filtering functionality with start values"""
+        csv_dat = ['"time";"a";"b";"c";"d"', \
+                   '"fmiReal";"fmiReal";"fmiInteger";"fmiBoolean";"fmiString"',\
+                   '0.0;;;;']
+        start={"a":0.5, "b":2, "c":True, "d":"Big Bang"}
+        source_reader = Reader(csv_dat)
+        filtered_reader = ConstantInterpolationEventFilter(source_reader, start)
+        
+        it = iter(filtered_reader)
+        
+        ev = next(it)
+        self.assertEqual(ev.get_time(), 0.0)
+        ref = {"a":0.5, "b":2, "c":True, "d":"Big Bang"}
+        self.assertDictEqual(dict(ev), ref)
+
+
 if __name__ == "__main__":
     unittest.main()
 
